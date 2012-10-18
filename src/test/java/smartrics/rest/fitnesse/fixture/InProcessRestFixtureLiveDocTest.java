1c1ac8923d04ceea8e1248de541a344b2b2a7f61
smartrics <smartrics@gmail.com>
2012-10-18 23:25:08 +0100

    added run of fit tests and slim tests + added import

--- src/test/java/smartrics/rest/fitnesse/fixture/InProcessRestFixtureLiveDocTest.java
+++ src/test/java/smartrics/rest/fitnesse/fixture/InProcessRestFixtureLiveDocTest.java
@@ -7,24 +7,25 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import fitnesse.junit.JUnitHelper;
-import fitnesse.responders.run.JavaFormatter;
-import fitnesse.responders.run.formatters.SuiteHtmlFormatter;
-import fitnesse.runner.HtmlResultFormatter;
 
-@Ignore
+//@Ignore
 public class InProcessRestFixtureLiveDocTest {
 
 	JUnitHelper helper;
 
 	@Before
 	public void prepare() {
-		helper = new JUnitHelper("src/main/resources", new File("target",
-				"fitnesse-junit").getAbsolutePath());
+		helper = new JUnitHelper("src/main/resources", new File("target", "fitnesse-junit").getAbsolutePath());
 		helper.setPort(9090);
 	}
 
 	@Test
 	public void restFixtureFitTests() throws Exception {
 		helper.assertSuitePasses("RestFixtureFitTests");
+	}
+
+	@Test
+	public void restFixtureTests() throws Exception {
+		helper.assertSuitePasses("RestFixtureTests.OverrideContentHandlers");
 	}
 }
