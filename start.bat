92a25adbec39f91829b15f7c19a2fbd4bf9d6c9e
smartrics <smartrics@gmail.com>
2016-02-25 00:31:05 +0000

    added to start fitnesse manually

---
+++ start.bat
@@ -0,0 +1,10 @@
+setLocal EnableDelayedExpansion
+set CLASSPATH="
+for /R target\dependencies %%a in (*.jar) do (
+   set CLASSPATH=!CLASSPATH!;%%a
+)
+set CLASSPATH=!CLASSPATH!"
+echo !CLASSPATH!
+ 
+java -cp !CLASSPATH! fitnesseMain.FitNesseMain -p 9090 -d src\main\resources -e 0 
+
