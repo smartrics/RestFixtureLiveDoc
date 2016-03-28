e76606fc3ad02e2d70568255d9f436059dc3ac2b
smartrics <smartrics@gmail.com>
2016-03-28 21:47:01 +0100

    Tested Issue #174 and proved it's working

--- src/main/java/smartrics/rest/test/fitnesse/fixture/ColumnFixtureTest.java
+++ src/main/java/smartrics/rest/test/fitnesse/fixture/ColumnFixtureTest.java
@@ -5,6 +5,10 @@ import fit.ColumnFixture;
 public class ColumnFixtureTest extends ColumnFixture {
     public String returnCode;
 
+    public void setReturnCode(String returnCode) {
+        this.returnCode = returnCode;
+    }
+
     public boolean isSuccess() {
         final int i = Integer.parseInt(returnCode);
         return i >= 200 && i <300;
--- src/main/java/smartrics/rest/test/fitnesse/fixture/DecisionFixtureTest.java
+++ src/main/java/smartrics/rest/test/fitnesse/fixture/DecisionFixtureTest.java
@@ -1,7 +1,18 @@
 package smartrics.rest.test.fitnesse.fixture;
 
 /**
+ * Utility fixture to test symbols
+ *
  * Created by 702161900 on 28/03/2016.
  */
 public class DecisionFixtureTest {
+    private String value;
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
 }
