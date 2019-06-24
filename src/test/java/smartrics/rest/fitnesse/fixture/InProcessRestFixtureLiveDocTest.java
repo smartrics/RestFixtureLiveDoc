787bcb2f6758528646a156b4abc00e8c133c36dc
smartrics <fabrizio.cannizzo@iotic-labs.com>
2019-06-24 22:28:09 +0100

    restored running of the entire suite

--- src/test/java/smartrics/rest/fitnesse/fixture/InProcessRestFixtureLiveDocTest.java
+++ src/test/java/smartrics/rest/fitnesse/fixture/InProcessRestFixtureLiveDocTest.java
@@ -20,6 +20,6 @@ public class InProcessRestFixtureLiveDocTest {
 
 	@Test
 	public void restFixtureTests() throws Exception {
-		helper.assertSuitePasses("RestFixtureTests.IssueFixes.Issue255");
+		helper.assertSuitePasses("RestFixtureTests");
 	}
 }
